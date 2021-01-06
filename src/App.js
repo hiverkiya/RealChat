b702c1115a59afc3d032fd409451173defcddfb7
Himanshu Verkiya <hiverkiya@gmail.com>
2021-01-06 20:25:43 +0530

    Minimal modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>
+{// We defined this component here but not rendering it here and exporting it in index.js
+  let element=<h1>Welcome to React</h1>
+  return element;
 }
 
 export default App;
